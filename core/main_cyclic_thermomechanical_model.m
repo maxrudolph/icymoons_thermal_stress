99c9f7ec29145fc289d08a8637beaa425c647ea0
Max Rudolph <maxrudolph@ucdavis.edu>
2020-12-06 18:49:13 -0800

    debug convergence issuse

--- core/main_cyclic_thermomechanical_model.m
+++ core/main_cyclic_thermomechanical_model.m
@@ -271,11 +271,11 @@ while time < t_end
 
         % check for convergence
         if abs( Pex_post-Pex )/abs(Pex) < 1e-4
-            fprintf('dt=%.2e yr, time=%.3e Myr, Pex_post %.12e Pex %.12e, converged in %d iterations\n',dt/seconds_in_year,(time+dt)/seconds_in_year/1e6,Pex_post,Pex,iter);
+            fprintf('dt=%.2e yr, time=%.3e Myr, Pex_post %.2e Pex %.2e, converged in %d iterations\n',dt/seconds_in_year,(time+dt)/seconds_in_year/1e6,Pex_post,Pex,iter);
             converged = true;
         elseif iter==maxiter
             for i=1:maxiter
-                 fprintf('iter %d. Pex_post %.6e Pex %.6e\n',i,pexpost_store(i),pex_store(i));
+                 fprintf('iter %d. Pex_post %.12e Pex %.12e\n',i,pexpost_store(i),pex_store(i));
             end
             error('Nonlinear loop failed to converge');            
         end
