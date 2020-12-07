5aef6baf271910313799a65f946f0491af10b361
Max Rudolph <maxrudolph@ucdavis.edu>
2020-12-06 18:46:26 -0800

    debug convergence issuse

--- core/main_cyclic_thermomechanical_model.m
+++ core/main_cyclic_thermomechanical_model.m
@@ -271,7 +271,7 @@ while time < t_end
 
         % check for convergence
         if abs( Pex_post-Pex )/abs(Pex) < 1e-6
-            fprintf('dt=%.2e yr, time=%.3e Myr, Pex_post %.6e Pex %.6e, converged in %d iterations\n',dt/seconds_in_year,(time+dt)/seconds_in_year/1e6,Pex_post,Pex,iter);
+            fprintf('dt=%.2e yr, time=%.3e Myr, Pex_post %.12e Pex %.12e, converged in %d iterations\n',dt/seconds_in_year,(time+dt)/seconds_in_year/1e6,Pex_post,Pex,iter);
             converged = true;
         elseif iter==maxiter
             for i=1:maxiter
