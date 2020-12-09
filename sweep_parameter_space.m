aa171398424dfb9cdfcbb31a7b9ce692940a0ada
Max Rudolph <maxrudolph@ucdavis.edu>
2020-12-09 11:15:03 -0800

    save more frequently to reduce scatter in phase lag plot

--- sweep_parameter_space.m
+++ sweep_parameter_space.m
@@ -13,13 +13,12 @@ for moon=0:1
         parameters.Ts = 100;
         parameters.g  = 1.30;
         parameters.Ro = 1.561e6;
-        parameters.Ri = parameters.Ro-4.0e3;     % inner radius of ice shell (m)
         parameters.Rc = parameters.Ro-1.2e5;     % core radius (m)
         parameters.relaxation_parameter = 1e-4;  % europa value
         parameters.tensile_strength = 3e6;
         parameters.perturbation_period = 1e8*seconds_in_year;
         parameters.save_start = parameters.perturbation_period*5;
-        parameters.save_interval = parameters.perturbation_period/100;
+        parameters.save_interval = parameters.perturbation_period/1000;
         parameters.end_time = parameters.perturbation_period*10;
         parameters.label = 'Europa';
     else
@@ -34,7 +33,7 @@ for moon=0:1
         parameters.tensile_strength = 3e6;
         parameters.perturbation_period = 1e8*seconds_in_year;
         parameters.save_start = parameters.perturbation_period*5;
-        parameters.save_interval = parameters.perturbation_period/100;
+        parameters.save_interval = parameters.perturbation_period/1000;
         parameters.end_time = parameters.perturbation_period*10;
         parameters.label = 'Enceladus';
     end
@@ -64,6 +63,7 @@ clearvars -except seconds_in_year;
 seconds_in_year = 3.1558e7;
 
 load('Enceladus_workspace.mat');
+% load('Europa_workspace.mat');
 
 %% Analyze models
 all_erupted_volume = zeros(size(all_results));
@@ -80,6 +80,8 @@ for ithick=1:nthick
     end
 end
 figure();
+addpath ~/sw/matlab/crameri
+
 t=tiledlayout(2,1,'Padding','none','TileSpacing','none','Units','centimeters');
 t.OuterPosition(3:4) = [9 9];
 nexttile;
@@ -87,19 +89,60 @@ dt = max(results.time) - 2*parameters.perturbation_period;
 contourf(thicknesses/1e3,dQ,all_erupted_volume/(4*pi*parameters.Ro^2)/(dt/seconds_in_year/1e6),16,'Color','none');
 hcb=colorbar();
 hcb.Label.String = 'Erupted Volume (m/Myr)';
-ylabel('\Delta Q/Q_0');
+colormap(crameri('davos'));
+ylabel('\Delta q/q_0');
 xlabel('Average Thickness (km)');
-
+if( max(all_erupted_volume(:))==0 )
+    caxis([0 1])
+end
+title(parameters.label);
 nexttile;
 dt = max(results.time) - 2*parameters.perturbation_period;
-contourf(thicknesses/1e3,dQ,all_failure_events/(dt/parameters.perturbation_period),24,'Color','none');
+contourf(thicknesses/1e3,dQ,all_failure_events/(dt/parameters.perturbation_period),16,'Color','none');
 hcb=colorbar();
+
 hcb.Label.String = 'Failure events per cycle';
-ylabel('\Delta Q/Q_0');
+ylabel('\Delta q/q_0');
 xlabel('Average Thickness (km)');
+colormap(crameri('davos'));
+exportgraphics(gcf,[parameters.label '_regimes.eps']);
 
-exportgraphics(gcf,'test.eps');
-
+%% Look at phase lag between qb and thickness
+all_phase_lags = zeros(ndQ,nthick);
+all_ampfrac = zeros(ndQ,nthick);
+for ithick=1:nthick
+    for idQ=1:ndQ
+        
+        rho_i = 900;
+        Cp = 2100;              % heat capacity of ice (J/kg/K)
+        kappa = 1e-6;           % Thermal diffusivity (m/s/s)
+        k=kappa*rho_i*Cp;
+        
+        results = all_results{idQ,ithick};
+        parameters = all_parameters{idQ,ithick};       
+        
+        actual_dz = (results.Ri-results.z) - mean(results.Ri-results.z);
+        actual_thickness = parameters.Ro - (results.Ri-results.z);
+        %         plot(results.time/seconds_in_year, (actual_dz/std(actual_dz)) );
+%         figure, plot(results.time/seconds_in_year, actual_thickness ); hold on
+        ss_thickness = k*(273-100)./results.qb;
+        [ss_peaks,ss_ind] = findpeaks(ss_thickness);
+        [actual_peaks,actual_ind] = findpeaks(actual_thickness);
+        phase_lag = median(results.time(actual_ind) - results.time(ss_ind))/seconds_in_year/1e6;
+        all_phase_lags(idQ,ithick) = phase_lag;
+        all_ampfrac(idQ,ithick) = (max(actual_thickness)-min(actual_thickness))/(max(ss_thickness)-min(ss_thickness));
+%         plot(results.time/seconds_in_year,ss_thickness,'--');
+    end
+end
+figure;
+subplot(2,1,1);
+contourf(thicknesses/1e3,dQ,all_phase_lags,16,'Color','none'); colorbar();
+ylabel('\Delta q/q_0');
+xlabel('Average Thickness (km)');
+subplot(2,1,2);
+contourf(thicknesses/1e3,dQ,all_ampfrac,16,'Color','none'); colorbar();
+ylabel('\Delta q/q_0');
+xlabel('Average Thickness (km)');
 %% Plotting routines:
 for ithick=1:1
     for idQ=1:3:ndQ
