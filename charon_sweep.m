47fdafd5f245c9db01c7da77fbb6c9f81a391fc9
Max Rudolph <maxrudolph@ucdavis.edu>
2022-07-06 13:46:27 -0700

    plots working

--- charon_sweep.m
+++ charon_sweep.m
@@ -69,13 +69,15 @@ ylabel('Initial thickness (km)');
 set(gca,'YDir','normal');
 
 figure();
-imagesc(Xs,thicknesses,all_max_failure_thickness);
-colorbar();
-caxis([0 15000])
+imagesc(Xs,thicknesses,all_max_failure_thickness/1e3);
+hcb=colorbar();
+hcb.Label.String = 'Thickest ice when cracks reach ocean (km)';
+caxis([0 15])
 xlabel('Initial ammonia content (X_0)');
 ylabel('Initial thickness (km)');
 set(gca,'YDir','normal');
-
+set(gcf,'Color','w');
+exportgraphics(gcf,'Charon-thickest-cracks.eps','ContentType','Vector');
 
 
 figure();
@@ -106,85 +108,97 @@ plot(result.time/seconds_in_year,result.Pex);
 ylabel('Overpressure (Pa)');
 
 %% Pseudocolor stress plot
-xscale = 'log';
-figure();
-t=tiledlayout(4,1,'TileSpacing','none','Padding','none');
-%         t.Units = 'centimeters';
-%         t.OuterPosition = [1 1 9.5 9];
-nexttile
-
-contourf(result.time/seconds_in_year,result.save_depths/1000,result.sigma_t,64,'Color','none'); %shading flat;
-hold on
-plot(result.time/seconds_in_year,((result.parameters.Ro-result.parameters.Ri)+result.z)/1000,'Color','k','LineWidth',1);
-%         set(gca,'YLim',[0 ceil(1+max(((Ro-results.Ri(mask))+results.z(mask))/1000))]);
-set(gca,'YDir','reverse');
-ax1 = gca();
-ax1.FontSize=8;
-hcb = colorbar();
-hcb.Label.String = 'Tensile Stress (Pa)';
-text(0.025,0.85,char('A'),'FontSize',8,'Units','normalized');
-xlabel('Time (years)');
-title(result.label);
-ylabel('Depth (km)');
-set(gca,'XScale',xscale);
-hold on;
-for i=1:length(result.failure_time)
-    plot(result.failure_time(i)*1e6*[1 1],[result.failure_top(i) result.failure_bottom(i)]/1e3,'r');
-end
-nexttile
-plot(result.time/seconds_in_year,result.Pex);
-ylabel('P_{ex} (Pa)');
-set(gca,'XScale',xscale);
-ax2 = gca();
-ax2.Position(3) = ax1.Position(3);
-ax2.XLim = ax1.XLim;
-ax2.FontSize=8;
-hold on
-plot(result.failure_time*1e6,result.failure_P,'r.');
-end_color = [0 0.9 0];
-plot(result.failure_time*1e6,(result.failure_P+result.failure_dP),'LineStyle','none','Color',end_color,'Marker','o','MarkerFaceColor',end_color,'MarkerSize',2);
-text(0.025,0.85,char('B'),'FontSize',8,'Units','normalized');
-plot(result.failure_time*1e6,result.failure_Pex_crit,'b--');
-
-
-xlabel('Time (years)');
-nexttile
-hold on;
-for i=1:length(result.failure_time)
-    if isnan(result.failure_erupted_volume(i))
-        % plot nothing
-    else
-        if result.failure_P(i) - result.failure_Pex_crit(i) > 0
-            plot(result.failure_time(i)*1e6*[1 1],[0 1],'b');
-        else
-            plot(result.failure_time(i)*1e6*[1 1],[0 1],'b--');
+for ithick = [1 nthick]
+    for iX = [1 nX]
+        
+        result = results{ithick,iX}
+        xscale = 'linear';
+        figure();
+        f=gcf();
+        f.Position(3:4) = [945 890];
+        t=tiledlayout(4,1,'TileSpacing','none','Padding','none');
+        t.Units = 'centimeters';
+        t.OuterPosition = [1 1 9 12];
+        nexttile
+        
+        contourf(result.time/seconds_in_year,result.save_depths/1000,result.sigma_t,64,'Color','none'); %shading flat;
+        hold on
+        plot(result.time/seconds_in_year,((result.parameters.Ro-result.parameters.Ri)+result.z)/1000,'Color','k','LineWidth',1);
+        %         set(gca,'YLim',[0 ceil(1+max(((Ro-results.Ri(mask))+results.z(mask))/1000))]);
+        set(gca,'YDir','reverse');
+        ax1 = gca();
+        ax1.FontSize=8;
+        hcb = colorbar();
+        hcb.Label.String = 'Tensile Stress (Pa)';
+        text(0.025,0.85,char('A'),'FontSize',8,'Units','normalized');
+%         xlabel('Time (years)');
+        title(result.label);
+        ylabel('Depth (km)');
+        set(gca,'XScale',xscale);
+        hold on;
+        for i=1:length(result.failure_time)
+            plot(result.failure_time(i)*1e6*[1 1],[result.failure_top(i) result.failure_bottom(i)]/1e3,'r');
         end
+        nexttile
+        plot(result.time/seconds_in_year,result.Pex);
+        ylabel('P_{ex} (Pa)');
+        set(gca,'XScale',xscale);
+         ax2 = gca();
+%         ax2.Position(3) = ax1.Position(3);
+         ax2.XLim = ax1.XLim;
+        ax2.FontSize=8;
+        hold on
+        plot(result.failure_time*1e6,result.failure_P,'r.');
+        end_color = [0 0.9 0];
+        plot(result.failure_time*1e6,(result.failure_P+result.failure_dP),'LineStyle','none','Color',end_color,'Marker','o','MarkerFaceColor',end_color,'MarkerSize',2);
+        text(0.025,0.85,char('B'),'FontSize',8,'Units','normalized');
+        plot(result.failure_time*1e6,result.failure_Pex_crit,'b--');
+        
+        
+%         xlabel('Time (years)');
+        nexttile
+        hold on;
+        for i=1:length(result.failure_time)
+            if isnan(result.failure_erupted_volume(i))
+                % plot nothing
+            else
+                if result.failure_P(i) - result.failure_Pex_crit(i) > 0
+                    plot(result.failure_time(i)*1e6*[1 1],[0 1],'b');
+                else
+                    plot(result.failure_time(i)*1e6*[1 1],[0 1],'b--');
+                end
+            end
+            %         plot(results.failure_time(i)*1e6,results.failure_erupted_volume_volumechange(i)/(4*pi*Ro^2),'go');
+            %         plot(results.failure_time(i)*1e6,results.failure_erupted_volume_pressurechange(i)/(4*pi*Ro^2),'rx');
+        end
+        ylabel('Eruption?');
+%         xlabel('Time (years)');
+        set(gca,'XScale',xscale);
+        ax3=gca();
+        ax3.XLim = ax1.XLim;
+%         ax3.Position(3) = ax1.Position(3);
+        ax3.Box = 'on';
+        ax3.FontSize=8;
+        text(0.025,0.85,char('C'),'FontSize',8,'Units','normalized');
+        
+        nexttile;
+        plot(result.time/seconds_in_year,result.XNH3(1:length(result.time)),'k');
+        set(gca,'XScale',xscale);
+        ylabel('X_{NH_3}')
+        xlabel('Time (years)');
+        
+        set(gca,'XLim',ax1.XLim);
+        set(gca,'XTick',ax1.XTick);
+
+                text(0.025,0.85,char('D'),'FontSize',8,'Units','normalized');
+
+        
+        fig = gcf();
+        %fig.PaperUnits = 'centimeters';
+        %fig.PaperPosition(3) = 6.00;
+        fig.Color = 'w';
+        filename = sprintf('%s_thickening_nh3-%f_h0-%f.eps',result.label,result.parameters.X0,...
+             result.parameters.Ro-result.parameters.Ri);
+        exportgraphics(t,filename,'ContentType','vector');
     end
-    %         plot(results.failure_time(i)*1e6,results.failure_erupted_volume_volumechange(i)/(4*pi*Ro^2),'go');
-    %         plot(results.failure_time(i)*1e6,results.failure_erupted_volume_pressurechange(i)/(4*pi*Ro^2),'rx');
 end
-ylabel('Eruption?');
-xlabel('Time (years)');
-set(gca,'XScale',xscale);
-ax3=gca();
-ax3.XLim = ax1.XLim;
-ax3.Position(3) = ax1.Position(3);
-ax3.Box = 'on';
-ax3.FontSize=8;
-text(0.025,0.85,char('C'),'FontSize',8,'Units','normalized');
-
-nexttile;
-plot(result.time/seconds_in_year,result.XNH3,'k');
-set(gca,'XScale',xscale);
-ylabel('X_{NH_3}')
-xlabel('Time (years)');
-set(gca,'XLim',ax1.XLim);
-
-fig = gcf();
-fig.PaperUnits = 'centimeters';
-fig.PaperPosition(3) = 6.00;
-fig.Color = 'w';
-% filename = sprintf('%s_thickening_nh3-%f_h0-%f.eps',label,initial_ammonia(iAmmonia),...
-%     thicknesses(ithick));
-% exportgraphics(gcf,filename,'ContentType','vector');
-
